29bd40ff211300febb6e43c1e01d95f45c50fe38
rafianathallah <71361919+rafianathallah@users.noreply.github.com>
2022-06-28 17:51:27 +0700

    updated code

--- backend.js
+++ backend.js
@@ -1,73 +1,74 @@
-const express = require('express');
-const morgan = require("morgan");
-const cors = require('cors');
-const { default: axios } = require('axios');
-const bodyParser = require('body-parser')
+// ****FOR TESTING****
+// const express = require('express');
+// const morgan = require("morgan");
+// const cors = require('cors');
+// const { default: axios } = require('axios');
+// const bodyParser = require('body-parser')
+// const app = express();
+// app.use(cors())
+// app.use(bodyParser.json({limit: '50mb'}))
+// app.use(bodyParser.urlencoded({extended:true}))
+// const PORT = 4000;
+// const HOST = "localhost";
+// app.use(morgan('dev'));
 
-// Create Express Server
-const app = express();
-
-app.use(cors())
-app.use(bodyParser.json({limit: '50mb'}))
-app.use(bodyParser.urlencoded({extended:true}))
-
-// Configuration
-const PORT = 4000;
-const HOST = "localhost";
-
-// Logging
-app.use(morgan('dev'));
-
-app.get("/", (req, res) => res.send("..."));
-
-let products = [
-    {
-        "name": "Arabica",
-        "price": 20
-    },
-    {
-        "name": "Robusta",
-        "price": 15
-    },
-]
-
-app.get('/products', (req, res) => {
-    res.send(products);
-})
-
-app.post('/products', (req, res) => {
-    const product = req.body;
-    products.push(product)
-    res.send('added')
-})
-
+//Add & show users
 let users = [
     {
-        "firstName": "John",
-        "lastName": "Doe"
+        "firstName": "Raiden",
+        "lastName": "Shogun"
     },
     {
-        "firstName": "LeBron",
-        "lastName": "James"
+        "firstName": "Yae",
+        "lastName": "Miko"
+    },
+    {
+        "firstName": "Kujou",
+        "lastName": "Sara"
+    },
+    {
+        "firstName": "Kuki",
+        "lastName": "Shinobu"
     }
 ]
 
 app.get('/users', (req, res) => {
     res.send(users);
 })
-
 app.post('/users', (req, res) => {
     const user = req.body;
     users.push(user)
-    res.send('added')
+    res.send('Successfully added')
 })
 
+//Add & show products
+app.get("/", (req, res) => res.send("..."));
+let coffees = [
+    {
+        "coffeeName": "Athenaeum",
+        "price": 8
+    },
+    {
+        "cofeeName": "Foamy Reef",
+        "price": 16
+    },
+    {
+        "cofeeName": "Golden Eden",
+        "price": 24
+    },
+    {
+        "cofeeName": "Caramel Pinecone",
+        "price": 44
+    }
+]
 
+app.get('/coffees', (req, res) => {
+    res.send(coffees);
+})
+app.post('/coffees', (req, res) => {
+    const coffee = req.body;
+    coffees.push(coffee)
+    res.send('Succesfully added')   
+})
 
-// Start the Proxy
-app.listen(PORT , HOST, () => {
-    console.log(`Starting Proxy, hopefully it workss`);
- });
- 
-
-
+// app.listen(PORT , HOST, () => {});
